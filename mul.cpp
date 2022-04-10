c51845f8ea5d8ff9140e0572ffb3b0dcfdc49107
Samueal Louis Dsouza <dsouzasamueal07@gmail.com>
2022-04-10 18:19:30 +0530

    inside ftplayer branch

---
+++ mul.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"yo yo kun"<< endl;
+    return 0;
+}
--- yo.cpp
+++ yo.cpp
@@ -4,5 +4,8 @@ using namespace std;
 
 int main(){
     cout<<"he is back"<<endl;
+    cout<< "antoine griezmann"<<endl;
+    return 0;
+
 }
 
